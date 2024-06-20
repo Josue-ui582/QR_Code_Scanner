45a360f0a549b7c0fd03b86e2b175cfddd2fd7dd
Josué Jésugnon HOUNHUOI <hounhouijosue1@gmail.com>
2024-06-20 16:02:51 +0200

    update

--- QR_Code/index.js
+++ QR_Code/index.js
@@ -1,22 +1,28 @@
 const wrapper = document.querySelector(".wrapper");
 const generateBtn = document.querySelector(".form button");
-const input = document.querySelector(".form input");
+const input1 = document.querySelector(".form .texts");
+const input2 = document.querySelector(".form .files");
 const qrImage = document.querySelector(".QR-Code img");
 
 generateBtn.addEventListener("click", () => {
-    let qrValue = input.value;
-    if(!qrValue) return; // The input is empty...
+    let qrValue = input1.value;
+    let qrValue2 = input2.files[0];
+    if(!qrValue && !qrValue2) return; // The input is empty...
     generateBtn.innerText = "Generating QR Code...";
     //Getting QR-Code for users
-    qrImage.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+    if (qrValue) {
+        qrImage.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+    }else {
+        qrImage.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue2}`
+    }
     qrImage.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
     });
 });
 
-input.addEventListener("keyup", () => {
-    if(!input.value) {
+input1.addEventListener("keyup", () => {
+    if(!input1.value) {
         wrapper.classList.remove("active");
     }
 })
