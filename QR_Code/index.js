c97ed48830499ef80feb949fbb38e9352d5d4cd1
Josué Jésugnon HOUNHUOI <hounhouijosue1@gmail.com>
2024-06-21 14:01:34 +0200

    update

--- QR_Code/index.js
+++ QR_Code/index.js
@@ -3,17 +3,27 @@ const generateBtn = document.querySelector(".form button");
 const input1 = document.querySelector(".form .texts");
 const input2 = document.querySelector(".form .files");
 const qrImage = document.querySelector(".QR-Code img");
+const errorMessage = document.querySelector('.error');
+const succes = document.querySelector('.succes');
 
 generateBtn.addEventListener("click", () => {
     let qrValue = input1.value;
     let qrValue2 = input2.files[0];
-    if(!qrValue && !qrValue2) return; // The input is empty...
+    if(!qrValue && !qrValue2) { 
+        errorMessage.classList.add('errorMessage');
+        succes.classList.remove("succesMessage");
+        return // Nothing is not return, the user should enter some input;
+    }
     generateBtn.innerText = "Generating QR Code...";
     //Getting QR-Code for users
     if (qrValue) {
         qrImage.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
-    }else {
+        errorMessage.classList.remove("errorMessage")
+        succes.classList.add('succesMessage');
+    }else if (qrValue2){
+        
         qrImage.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue2}`
+        succes.classList.add('succesMessage');
     }
     qrImage.addEventListener("load", () => {
         wrapper.classList.add("active");
@@ -22,7 +32,8 @@ generateBtn.addEventListener("click", () => {
 });
 
 input1.addEventListener("keyup", () => {
-    if(!input1.value) {
+    if(!input1.value && !input2.value) {
         wrapper.classList.remove("active");
+        succes.classList.remove("succesMessage");
     }
 })
